a031beb0efe395f2ec11c7c2d3d7c2d92b407db2
yanawijaya1 <hidayanaikenur@gmail.com>
2024-08-17 18:25:24 +0700

    proxy

---
+++ utils/deployed-address.ts
@@ -0,0 +1,4 @@
+export const SWTRProxy = '0xB90993233d647b6c2d8e2106D4eB5F37a02fe0d7'
+export const ProxyAdmin = '0x6CE894F33a65d135Bd1caB4D7C15Ed9b667d19B6'
+export const SWTRImplementation = '0x29b4C9B25FdEcbBBc3AA2EC3BF27e66216DEf93C'
+    
